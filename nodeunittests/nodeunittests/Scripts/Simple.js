8eacefe32e839678c971f7c56c152ca8c39b27f6
Grant Spiteri <gdspiteri@live.com>
2016-05-20 06:55:42 +1000

    added tests

---
+++ nodeunittests/nodeunittests/Scripts/Simple.js
@@ -0,0 +1,16 @@
+﻿function Simple() {
+    this.someText = "Hello there";
+    this.someData = [
+        { name: "Martin Fowler", age: 34 },
+        { name: "Kam the master", age: 65 },
+        { name: "Jordan the batter", age: 22 }
+    ];
+}
+Simple.prototype.famousNames = function () {
+    var retval = [];
+    this.someData.forEach(function (value, index) {
+        retval.push(value);
+    });
+    return retval;
+}
+module.exports = Simple;
---
+++ nodeunittests/nodeunittests/Tests/simple.test.js
@@ -0,0 +1,18 @@
+﻿var assert = require("assert");
+var simple = require("../src/Simple");
+var simpleObject = new simple();
+describe("Simple Tests", function () {
+    describe("Constructor Test", function () {
+        it("Object is created", function () {
+            assert.ok(true);
+        }),
+        it("Has some text", function () {
+            assert.equal("Hello there",simpleObject.someText);
+        });
+    }),
+    describe("Famous Names", function () {
+        it("Has three", function () {
+            assert.equal(3, simpleObject.famousNames().length);
+        });
+    });
+});
